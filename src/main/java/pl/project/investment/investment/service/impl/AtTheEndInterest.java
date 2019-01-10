f3e2e70f51d982565a553d903f186f68f3f73a6a
Grzegorz Kuliński <kulinski.grzegorz90@gmail.com>
2019-01-10 11:07:57 +0100

    Fix structure

--- src/main/java/pl/project/investment/investment/service/impl/AtTheEndInterest.java
+++ src/main/java/pl/project/investment/investment/service/impl/AtTheEndInterest.java
@@ -2,8 +2,6 @@ package pl.project.investment.investment.service.impl;
 import org.decimal4j.util.DoubleRounder;
 import org.springframework.stereotype.Component;
 import pl.project.investment.investment.enums.TypeImplementation;
-import pl.project.investment.investment.service.CalculationInterface;
-import pl.project.investment.investment.service.ValidationService;
 
 /**
  * Class implementing calculationInterface 
@@ -11,13 +9,12 @@ import pl.project.investment.investment.service.ValidationService;
  * @author Kuliński
  */
 @Component
-public class AtTheEndInterest extends ValidationService implements CalculationInterface {
+public class AtTheEndInterest extends CalculationImpl {
 
 	private TypeImplementation type = TypeImplementation.EndAlgorithm;
 
 	@Override
-	public double calculateInterest(int days, double interest, double amount) {
-		super.isValueLogic(days,interest,amount);
+	double doCalc(int days, double interest, double amount) {
 		double result = amount * (((interest / 100)) * ((days / DAYS_IN_MONTH) / 12));
 		return DoubleRounder.round(result, 2);
 	}

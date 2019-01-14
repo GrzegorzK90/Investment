0f0253fd75bb41e69baff4b2c2de5ca2caaa5094
Grzegorz Kuliński <kulinski.grzegorz90@gmail.com>
2019-01-14 15:57:55 +0100

    clean project

--- target/classes/clear.sql
+++ target/classes/clear.sql
@@ -1,5 +1,5 @@
 ﻿delete from calculation;
 delete from investment;
 
-insert into investment values(1,4.0,'2018-10-10', '2018-11-10', 'Lokata');
+insert into investment values(1,4.0,'2018-10-10', '2018-11-10', 'InvestmentTest');
 insert into calculation values(1,1000.00,'2018-12-12',3.33,1);
